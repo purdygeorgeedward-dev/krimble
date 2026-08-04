857c6bf45a5fad7333cadb4db5647c838e2dfc87
George Edward Purdy <purdygeorgeedward@gmail.com>
2026-08-04 15:59:18 -0600

    Set toolbox priority for zoom tool

--- libs/flake/tools/KoZoomToolFactory.cpp
+++ libs/flake/tools/KoZoomToolFactory.cpp
@@ -17,6 +17,7 @@ KoZoomToolFactory::KoZoomToolFactory()
     setToolTip(i18n("Zoom"));
     setSection(ToolBoxSection::Navigation);
     setPriority(0);
+    setToolBoxPriority(1);
     setIconName(koIconNameCStr("tool_zoom"));
     setActivationShapeId("flake/always");
 }
